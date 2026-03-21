19466fdc89ed50dcd41fb90d2dd61ac089fe03c8
Jonathan Sánchez Muñoz <jonathan.munoz@ibotta.com>
2026-03-21 14:41:06 -0600

    Added debug mode to web variants

--- composeApp/src/jsMain/kotlin/com/vibely/pos/Main.kt
+++ composeApp/src/jsMain/kotlin/com/vibely/pos/Main.kt
@@ -2,10 +2,14 @@ package com.vibely.pos
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.vibely.pos.config.DebugConfig
 import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    if (js("new URLSearchParams(window.location.search).get('debug') === 'true'") as Boolean) {
+        DebugConfig.enableDebugMode()
+    }
     ComposeViewport(document.body!!) {
         App()
     }
--- composeApp/src/wasmJsMain/kotlin/com/vibely/pos/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/vibely/pos/Main.kt
@@ -1,11 +1,20 @@
+@file:OptIn(ExperimentalWasmJsInterop::class)
+
 package com.vibely.pos
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.vibely.pos.config.DebugConfig
 import kotlinx.browser.document
+
+@JsFun("() => new URLSearchParams(window.location.search).get('debug') === 'true' ? 1 : 0")
+private external fun jsIsDebugMode(): Int
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    if (jsIsDebugMode() != 0) {
+        DebugConfig.enableDebugMode()
+    }
     ComposeViewport(document.body!!) {
         App()
     }
